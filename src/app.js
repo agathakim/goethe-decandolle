8602ffdd8e6108bb9f94db357f8d9c3c77b1782e
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2019-10-03 15:03:43 -0500

    remove redux

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {Provider} from 'react-redux';
-//
-// import AppState from './reducers/index';
 
 import '../node_modules/react-vis/dist/style.css';
 import './stylesheets/main.css';
